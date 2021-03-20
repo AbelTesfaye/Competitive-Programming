fd2f0f3274a1c052e26b9b0d5962b198619ba1a5
Abel Tesfaye <abeltesfaye45@gmail.com>
2021-03-20 19:05:09 +0300

    week 8: update quickSort

--- week 8/quickSort.cpp
+++ week 8/quickSort.cpp
@@ -1,13 +1,55 @@
-vector<int> quickSort(vector<int> arr) {
-        
-    swap(arr[0], arr[arr.size() - 1]);
-    
-    int l = 0;
-    for(int r = 0; r < arr.size(); r++){
-        if(arr[r] <= arr.back()){
-            swap(arr[l], arr[r]);
-            l++;
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void quickSort(vector <int> &arr) {
+	// Complete this function
+    if (arr.size() < 2) return;
+
+    vector <int> l, r;
+
+    int pivot = arr[0];
+
+    for (int i = 1; i < arr.size(); ++i) {
+        if (arr[i] <= pivot) {
+            l.push_back(arr[i]);
+        }
+        else {
+            r.push_back(arr[i]);
         }
     }
-    return arr;
+    quickSort(l);
+    quickSort(r);
+
+    int pi = 0;
+
+    for (int i = 0; i < l.size(); ++i) {
+        arr[pi++] = l[i];
+        cout << l[i] << " ";
+    }
+
+    arr[pi++] = pivot;
+    cout << pivot << " ";
+
+    for (int i = 0; i < r.size(); ++i) {
+        arr[pi++] = r[i];
+        cout << r[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector <int> arr(n);
+    for(int i = 0; i < (int)n; ++i) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr);
+
+    return 0;
 }
